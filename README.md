a73a3f0c9e2a6fee095ef135628276b6cdca5db6
kevinyamauchi <kevin.yamauchi@gmail.com>
2014-09-12 13:00:54 -0700

    All files complete. Haven't tested...'

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ I generated the python scripts using:
 ```
 thrift -r --gen py tutorial.thrift
 ```
+
+The client and server scripts assume that they are in the ThriftTutorial directory. Additionally the Thrift Python bindings must be installed. I installed them through pip.
+
+```
+sudo pip install thrift
+
+```
+
+I had to add the thrift source folder to  the ThriftTutorial directory to add the libraries...I need to find where pip installed them...
+
+
---
+++ client.py
@@ -0,0 +1,66 @@
+import sys, glob
+
+# Add the generated python files to the path
+sys.path.append('gen-py')
+sys.path.insert(0, glob.blog('../../lib/py/build/lib.*')[0])
+
+from tutorial import Calculator
+from tutorial.ttypes import *
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+try:
+    # Make socket
+    transport = TSocket.TSocket('localhost', 9090)
+
+    # Buffering is required because the socket is slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    client = Calculator.Client(protocol)
+
+    # Connect
+    transport.open()
+
+    client.ping()
+    print 'ping()'
+
+    # Try simple addition
+    sum = client.add(1, 1)
+    print '1 + 1 = %d' % (sum)
+
+    work = Work()
+    work.op = Operation.DIVIDE
+    work.num1 = 1
+    work.num2 = 0
+
+    try:
+        quotient = client.calculate(1, work)
+        print 'You divided by zero...'
+
+    except InvalidOperation, io:
+        print 'InvalidOperation: %r' % io
+
+    work.op = Operation.SUBTRACT
+    work.num1 = 15
+    work.num2 = 10
+
+    diff = client.calculate(1, work)
+    print '15 - 10 = %d' % (diff)
+
+    log = client.getStruct(1)
+    print 'Check log: %s' % (log.value)
+
+    # Close the connection
+    transport.close()
+
+except Thrift.TException, tx:
+    print '%s' % (tx.message)
+
+
